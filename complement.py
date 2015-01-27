73fc8a6f3ec4f4a132ff278387675467aa04bdc6
David Han <dh4031@yahoo.com>
2015-01-27 12:48:01 +0000

    This is the complementary DNA exercise file.

---
+++ complement.py
@@ -0,0 +1,12 @@
+my_dna = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+replacement1 = my_dna.replace('A', 'T') 
+replacement2 = replacement1.replace('T', 'A')  
+replacement3 = replacement2.replace('C', 'G') 
+replacement4 = replacement3.replace('G', 'C') 
+
+print(replacement1)
+print(replacement2)
+print(replacement3)
+print(replacement4)
+
+
