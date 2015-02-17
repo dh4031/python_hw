d708979b8b841b0b39cf515fe24533ae6f5fc016
David Han <dh4031@yahoo.com>
2015-02-17 15:18:46 +0000

    added [1-800.html] and stripping string

--- webscrape.py
+++ webscrape.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(open("634.html"))
+soup = BeautifulSoup(open("[1-800].html"))
 
 div_id = soup
 column = div_id.find_all(id="secondary-nav")
@@ -8,4 +8,4 @@ column = div_id.find_all(id="secondary-nav")
 for nav in column:
 	nav = nav.find("p", class_="subnavlinks")
 	names = p.a
-	print names.string
+	print names.string.rstrip(s["ECOEVOPUB SERIES"])
